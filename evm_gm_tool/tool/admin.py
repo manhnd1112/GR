02be6b2213772f5917acf36e0721f52da219f7ed
manhnd1112 <manhnd@kaopiz.com>
2018-03-31 02:25:13 -0400

    Update migration

--- evm_gm_tool/tool/admin.py
+++ evm_gm_tool/tool/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Office, User, Project, ProjectMember
+from .models import UserProfile, Project, ProjectMember
 
-admin.site.register(Office)
-admin.site.register(User)
+admin.site.register(UserProfile)
 admin.site.register(Project)
 admin.site.register(ProjectMember)
--- evm_gm_tool/tool/models.py
+++ evm_gm_tool/tool/models.py
@@ -1,37 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class Office(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    desc = models.CharField(max_length=500)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to="profile_image", blank=True)
+    desc = models.TextField(default='', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     class Meta:
-        db_table = "office"
-
-class User(models.Model):
-    email = models.CharField(max_length=255, unique=True)    
-    username = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-    firstname = models.CharField(max_length=255) 
-    lastname = models.CharField(max_length=255) 
-    avatar_url = models.CharField(max_length=255)
-    desc = models.TextField()
-    is_admin = models.BooleanField(default=False)
-    office_id = models.ForeignKey(Office, on_delete=models.DO_NOTHING)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    class Meta:
-        db_table = "user"
+        db_table = "user_profile"
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
-    desc = models.TextField()
-    status =  models.TextField() # save array of json objects: [{'AT': 1, 'PV': 10, 'EV': 7, 'AC':12}]
-    budget = models.FloatField()
-    pd = models.FloatField() 
-    created_user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    desc = models.TextField(default='', blank=True)
+    status =  models.TextField(default='', blank=True) # save array of json objects: [{'AT': 1, 'PV': 10, 'EV': 7, 'AC':12}]
+    budget = models.FloatField(default=0)
+    pd = models.FloatField(default=0) 
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     class Meta:
@@ -40,7 +26,7 @@ class Project(models.Model):
 class ProjectMember(models.Model):
     project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    access = models.IntegerField() # ADMIN (delete, write, add member), WRITE, READ
+    access = models.IntegerField() # ADMIN (full access), WRITE, READ
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     class Meta:
